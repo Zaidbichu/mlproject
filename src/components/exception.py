d4de2491298dc47004a2573ad925348d0f9d08ea
zaidbichu <zaidbichu4@gmail.com>
2026-01-17 22:46:49 -0700

    logger and exception

---
+++ src/components/exception.py
@@ -0,0 +1,12 @@
+import sys
+def error_message_details(error,error_details:sys):
+    _,_,error_tb=error_details.exc_info()
+    file_name=error_tb.tb_frame.f_code.co_filename
+    error_message="error occured in python script name{[0]} on the line number {[1]} and error message[{2}]".format(file_name,error_tb.tb_lineno,str(error))
+    return error_message
+class custom_exception(Exception):
+    def __init__(self, error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_details(error_message,error_details=error_details)
+    def __str__(self):
+        return self.error_message
