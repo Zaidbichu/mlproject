1045b6fa63f8e2e3acef07eeea775f6b2d153efd
zaidbichu <zaidbichu4@gmail.com>
2026-01-20 10:57:55 -0700

    'model_training'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import Data_transformation
 from src.components.data_transformation import Datatransformationconfig
+from src.components.model_training import modeltrainerconfig
+from src.components.model_training import modeltrainer
 @dataclass 
 class Dataingestionconfig:
     train_data_path:str=os.path.join('articraft','train.csv')
@@ -38,3 +40,5 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=Data_transformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_trainer=modeltrainer()
+    modeltrainer.initiated_model_trainer(train_arr,test_arr)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,26 @@ import os
 import sys
 import dill
 from src.exception import custom_exception
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            y_train_pred=model.predict(x_train)
+            y_test__pred=model.predict(x_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test__pred)
+            report[list(models.keys())[i]]=test_model_score
+        return report
+    except Exception as e:
+        raise custom_exception(e,sys)
 def save_obj(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
